2d35eed32abf4781bbdf6bffafeca7565db43962
tejaswini1995 <50068573+tejaswini1995@users.noreply.github.com>
2019-06-26 13:20:56 +0530

    Add files via upload

---
+++ customerservice.js
@@ -0,0 +1,20 @@
+var http=require('http');
+function process_request(req,resp)
+			
+			{
+	resp.writeHead(200,{'Content-Type':'text/html'});
+var customers=[
+{id:1,name:"John",city:"Mumbai"},
+	{id:2,name:"Jack",city:"Pune"},
+		{id:3,name:"Bela",city:"Banglore"},
+			{id:4,name:"Amy",city:"Chennai"}
+			];
+			
+			
+	resp.end(JSON.stringify(customers));
+			};	
+			var controller=('/customers');
+var server=http.createServer(process_request);
+server.listen(3000);
+console.log("server is listening at port 3000");
+			
